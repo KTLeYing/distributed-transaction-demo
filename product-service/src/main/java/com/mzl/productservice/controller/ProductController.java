030dd6e5b325e2e2f0e31f7a253b17ab0155cc8a
mazhenle <2198902814@qq.com>
2024-07-26 15:23:41 +0800

    feat: 新增用户和商品模块

--- product-service/src/main/java/com/mzl/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/mzl/productservice/controller/ProductController.java
@@ -1,17 +1,17 @@
-package com.mzl.commonservice.generator.controller;
+package com.mzl.productservice.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 用户表 前端控制器
+ * 商品表 前端控制器
  *
  * @author mzl
  * @since 2024-07-25
  */
 @RestController
-@RequestMapping("/user")
-public class UserController {
+@RequestMapping("/product")
+public class ProductController {
 
 }
 
--- user-service/src/main/java/com/mzl/userservice/controller/UserController.java
+++ user-service/src/main/java/com/mzl/userservice/controller/UserController.java
@@ -1,17 +1,17 @@
-package com.mzl.commonservice.generator.controller;
+package com.mzl.userservice.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 商品表 前端控制器
+ * 用户表 前端控制器
  *
  * @author mzl
  * @since 2024-07-25
  */
 @RestController
-@RequestMapping("/product")
-public class ProductController {
+@RequestMapping("/user")
+public class UserController {
 
 }
 
