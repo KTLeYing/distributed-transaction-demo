b2e11600805d85a999dadd2e41321ab5a2fd5059
mazhenle <2198902814@qq.com>
2024-07-26 15:24:29 +0800

    feat: 新增用户和商品模块

---
+++ product-service/src/test/java/com/mzl/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzl.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
